b26099f7e0c0757075049f4112fcb72854c458a3
Francisco Daniel García López <124075687+D-GaLo@users.noreply.github.com>
2025-04-24 19:32:01 -0600

    [Feature/InR&B-Corrección de revisión]
    Se han corregido los errores que se señalaron en la revisión de la versión 1.0.0 del FrontEnd

--- src/index.js
+++ src/index.js
@@ -2,38 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import{
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Cuenta from './paginas/Cuenta';
-import Ayuda from './paginas/Ayuda';
+import RegistrarIncidente from './paginas/RegistrarIncidente';
 import ActualizarDatos from './componentes/ActualizarDatos/ActualizarDatos';
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>
-    },
-
-    {
-        path: "cuenta",
-        element: <Cuenta/>
-    },
-
-    {
-        path: "/actualizar-datos",
-        element: <ActualizarDatos />,
-    },
-
-    {
-        path: "ayuda",
-        element: <Ayuda/>
-    },
+  { path: '/', element: <App /> },
+  { path: '/cuenta', element: <Cuenta /> },
+  { path: '/actualizar-datos', element: <ActualizarDatos /> },
+  { path: '/registrar_incidente', element: <RegistrarIncidente /> },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <RouterProvider router={router} />
-);
-
+  <RouterProvider router={router} />
+)
